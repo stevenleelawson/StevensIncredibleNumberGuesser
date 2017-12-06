8f7ff9aa681a1f076b2e7c7c89f496ed4678bacd
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-05 17:50:03 -0700

    last com before burn down

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
 var randomNum = Math.floor((Math.random() * 100 ) + 1);
+console.log(randomNum);
 var clearButton = document.getElementById('clear');
 var input = document.getElementById('main-input');
 var form = document.getElementById('form');
 var guessButton = document.getElementById('guess');
-var guess = input.value;
+var highLow = document.querySelector('.high-low');
+var p = document.querySelector('p');
+var userGuess = document.querySelector('#userGuess');
+
+function validator () {
+  var guessAsNumb = parseInt(input.value);
+  if (isNaN(guessAsNumb))  {
+    console.log(guessAsNumb);
+    highLow.innerText ='Please enter a number';
+    form.reset()
+  } else if (guessAsNumb > 100 || guessAsNumb < 1){
+    highLow.innerText = 'please enter a number between 1 and 100';
+  }
+}
+
+
 
   clearButton.addEventListener('click',function(e){
     form.reset();
     e.preventDefault();
-    var elem = document.querySelector('#userGuess');
-    elem.parentNode.removeChild(elem);
+    // userGuess.parentNode.removeChild(elem);
   });
 
-  guessButton.addEventListener('click',function(e){
-    var guessAsNumb = parseInt(input.value);
-    var el = document.getElementById('info');
+  guessButton.addEventListener('click',function(event){
+    highLow.innerText = "That's TOOooooooooo highlow";
+    p.innerText = guessAsNumb;
+    // var el = document.getElementById('info');
     // el.innerHTML = '<h2><p id="userGuess">' + guessAsNumb + '<p></h2>';
-    e.preventDefault();
-    if (isNaN(guessAsNumb))  {
-      alert("Pleaser enter  an numberer");
-      form.reset()
-    } else if (guessAsNumb > 100 || guessAsNumb < 1){
-      alert('please enter a number between 1 and 100');
-    }
-  })
-  guessButton.addEventListener('click', function(e){
-    var guessAsNumb = parseInt(input.value);
-    var randomNum = Math.floor((Math.random() * 100 ) + 1);
-    console.log(randomNum);
-    console.log(guessAsNumb);
-    var el = document.getElementById('info');
-    if(guessAsNumb > randomNum){
-      alert('too high');
-      document.getElementById('appendingDom').innerHTML = '<h2>Your Last Guess was: <p>' + guessAsNumb + '</p><h6>Too high, fool!</h6>';
-    } else if (guessAsNumb < randomNum){
-      document.getElementById('appendingDom').innerHTML = '<h2>Your Last Guess was: <p>' + guessAsNumb + '</p><h6>Too low, schmoo!</h6>';
-
-      alert('too low');
-    } else {
-      el.innerHTML ='<h2>BOOM</h2>';
-    }
-    // el.innerHTML = '<h2><p id="userGuess">' + guessAsNumb + '<p></h2>';
-    e.preventDefault();
+    event.preventDefault();
+    validator();
   })
