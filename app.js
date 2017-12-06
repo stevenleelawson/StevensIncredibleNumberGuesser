349cc99b11db72576c437dd94b91e9d2c1f9e791
Steven Lawson <unluckygamblermusic@gmail.com>
2017-12-05 19:48:30 -0700

    cookin wit gas naaaaw

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 var randomNum = Math.floor((Math.random() * 100 ) + 1);
 console.log(randomNum);
-var clearButton = document.getElementById('clear');
-var input = document.getElementById('main-input');
-var form = document.getElementById('form');
-var guessButton = document.getElementById('guess');
+var clearButton = document.querySelector('#clear-button');
+var guessButton = document.querySelector('#guess-button')
+var mainInput = document.querySelector('#main-input');
+var displayNumber = document.querySelector('.display-number');
 var highLow = document.querySelector('.high-low');
-var p = document.querySelector('p');
-var userGuess = document.querySelector('#userGuess');
 
-function validator () {
-  var guessAsNumb = parseInt(input.value);
-  if (isNaN(guessAsNumb))  {
-    console.log(guessAsNumb);
-    highLow.innerText ='Please enter a number';
-    form.reset()
-  } else if (guessAsNumb > 100 || guessAsNumb < 1){
-    highLow.innerText = 'please enter a number between 1 and 100';
-  }
-}
-
-
-
-  clearButton.addEventListener('click',function(e){
+clearButton.addEventListener('click',function(event){
+    event.preventDefault();
+    console.log(mainInput);
     form.reset();
-    e.preventDefault();
     // userGuess.parentNode.removeChild(elem);
   });
 
-  guessButton.addEventListener('click',function(event){
-    highLow.innerText = "That's TOOooooooooo highlow";
-    p.innerText = guessAsNumb;
-    // var el = document.getElementById('info');
-    // el.innerHTML = '<h2><p id="userGuess">' + guessAsNumb + '<p></h2>';
-    event.preventDefault();
-    validator();
-  })
+guessButton.addEventListener('click', function(event){
+  event.preventDefault();
+  var userGuess = mainInput.value;
+  var usingGuess = parseInt(userGuess);
+  displayNumber.innerText = usingGuess;
+  if (usingGuess < randomNum){
+    var userGuess = mainInput.value;
+    var usingGuess = parseInt(userGuess);
+    highLow.innerText = "Your Guess is too Low.";
+  } else if (usingGuess > randomNum){
+    highLow.innerText = "Your Guess is too High.";
+  } else if (usingGuess === randomNum){
+    highLow.innerText = "BOOM";
+  }
+  if (isNaN(usingGuess)){
+    displayNumber.innerText = "??";
+    highLow.innerText = "Enter an actual number, bitte";
+  } else if (usingGuess < 1 || usingGuess > 100){
+    highLow.innerText = "Please enter a number between zero and one hundred";
+  }
+})
